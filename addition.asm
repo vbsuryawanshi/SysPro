28a522483b235ee735515f51fad04f89aa5ef450
vbsuryawanshi <vaibhavbsuryawanshi@gmail.com>
2018-07-28 21:22:05 +0530

    first commi

---
+++ addition.asm
@@ -0,0 +1,18 @@
+section .data
+	a dd 10
+	b dd 20
+	msg db "Addition is : %d ",10,0 
+section .text
+	global main
+	extern printf,scanf
+main:
+	mov eax,dword[a]
+	mov edx,dword[b]
+
+	add eax,edx
+
+	push eax
+	push msg
+	call printf
+	add esp,8
+	
